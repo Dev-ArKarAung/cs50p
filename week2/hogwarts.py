1855ddca90076ef2137b5e72a284cf7821863814
Arkar <dev.arkaraung@gmail.com>
2026-08-03 17:56:00 +0700

    Week 2 - list short session

---
+++ week2/hogwarts.py
@@ -0,0 +1,15 @@
+students = ["Hermione", "Harry", "Ron"]
+
+for student in students:
+    print(student)
+
+
+
+
+
+
+
+
+# print(students[0]) # Output: Hermione
+# print(students[1]) # Output: Harry
+# print(students[2]) # Output: Ron
