1b3971663de0a88f4cb10451b7971b9849267424
Arkar <dev.arkaraung@gmail.com>
2026-08-16 17:37:35 +0700

    Week 4 started

---
+++ week4/generate.py
@@ -0,0 +1,32 @@
+import random
+
+cards = ["jack", "Queen", "King"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# number = random.randint(1, 10)
+# print(number)
+
+
+
+
+
+
+
+
+
+# coin = random.choice(["heads" , "tails"])
+# print(coin)
