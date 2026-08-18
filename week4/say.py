372ba0eea7ca60877c1d3d38877c79def1d0065c
Arkar <dev.arkaraung@gmail.com>
2026-08-18 20:16:48 +0700

    Week4 lessons completed

---
+++ week4/say.py
@@ -0,0 +1,23 @@
+import sys
+
+from sayings import hello
+
+
+if len(sys.argv) == 2:
+    hello(sys.argv[1])
+
+
+
+
+
+
+
+
+
+
+#Cowsay introduction
+# import cowsay
+# import sys
+
+# if len(sys.argv) == 2:
+#     cowsay.trex("hello,  + " + sys.argv[1])
