ecac2cf3541689cb5a88429e9cfc30110757d7fb
Arkar <dev.arkaraung@gmail.com>
2026-09-04 16:32:07 +0700

    Week6 short lesson

---
+++ week6/names.py
@@ -0,0 +1,33 @@
+name = input("What's your name? ")
+
+file = open("names.txt", "a")
+file.write(f"{name}\n")
+file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# names = []
+
+
+# for _ in range(3):
+#     names.append(input("What's your name?: "))
+
+
+# for name in sorted(names):
+#     print(f"hello, {name}")
