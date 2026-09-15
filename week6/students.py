50dbcac58adb2e022df49bf37a2cd2ddfd8b2d4f
Arkar <dev.arkaraung@gmail.com>
2026-09-15 20:52:13 +0700

    Week6 Lesson Completed

--- week6/students.py
+++ week6/students.py
@@ -1,16 +1,39 @@
-students = []
+import csv
 
-with open("students.csv") as file:
-    for line in file:
-        name, house = line.strip().split(",")
-        student = {"name": name, "house": house}
-        students.append(student)
+name = input("What's your name? ")
+home = input("Where's your home? ")
 
-def get_name(student):
-    return student["name"]
+with open("students.csv", "a", newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
 
-for student in sorted(students, key=get_name):
-    print(f"{student['name']} is in {student['house']}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import csv
+
+# students = []
+
+# with open("students.csv") as file:
+#     reader = csv.DictReader(file)
+#     for row in reader:
+#         students.append({"name": row["name"], "home": row["home"]})
+
+# for student in sorted(students, key=lambda student: student["name"]):
+#     print(f"{student['name']} is in {student['home']}")
 
 
 
