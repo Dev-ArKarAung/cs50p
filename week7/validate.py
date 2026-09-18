712f7800aeb83b442051b3e5c71bd36c3de75f27
Arkar <dev.arkaraung@gmail.com>
2026-09-19 00:53:22 +0700

    Week7 lesson started

---
+++ week7/validate.py
@@ -0,0 +1,33 @@
+import re
+
+email = input("What's your email?: ").strip()
+
+if re.search(r"^[^@]+@[^@]+\.edu$", email):
+    print("Valid")
+else:
+    print("Invalid")
+
+
+
+
+
+
+
+
+# email = input("What's your email?: ").strip()
+
+# username, domain = email.split("@")
+
+# if username and domain.endswith(".edu"):
+#     print("Valid")
+# else:
+#     print("Invalid")
+
+#____________________________________________
+
+# email = input("What's your email?: ").strip()
+
+# if "@" in email and "." in email:
+#     print("Valid")
+# else:
+#     print("Invalid")
