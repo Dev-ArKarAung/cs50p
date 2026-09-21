f2a17a789e97b57e3a5122c381200768d69baa36
Arkar <dev.arkaraung@gmail.com>
2026-09-21 09:29:38 +0700

    Week7 completed

---
+++ week7/twitter.py
@@ -0,0 +1,22 @@
+import re
+
+url = input("URL: ").strip()
+
+if matches := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE):
+    print(f"Username: ", matches.group(1))
+
+
+
+
+
+
+
+
+
+# username= re.sub(r"^(https?://)?(www\.)?twitter\.com/", "", url)
+# print(f"Username: {username}")
+
+#______________________________________
+
+# username = url.replace("https://twitter.com/", "")
+# print(f"Username: {username}")
