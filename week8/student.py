421e56f15a0d512c6ea1e03fe13e17543ecfc5a1
Arkar <dev.arkaraung@gmail.com>
2026-09-25 10:34:10 +0700

    Week 8 lesson completed

--- week8/student.py
+++ week8/student.py
@@ -1,28 +1,20 @@
 class Student:
     def __init__(self, name, house):
-        if not name:
-            raise ValueError("Missing name")
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
         self.name = name 
         self.house = house
 
     def __str__(self):
         return f"{self.name} from {self.house}"
 
-    
-
-
+    @classmethod
+    def get(cls):
+        name = input("Name: ")
+        house = input("House: ")
+        return cls(name, house)
 
 def main():
-    student = get_student()
+    student = Student.get()
     print(student)
-
-def get_student():
-    name = input("Name:")
-    house = input("House: ")
-    return Student(name, house)
-
 
 
 if __name__ == "__main__":
@@ -64,3 +56,24 @@ if __name__ == "__main__":
         #             return "🐶"
         #         case _:
         #             return "🪄"
+
+    # if I want to use property
+    # @property
+        # def name(self):
+        #      return self._name
+    
+        # @name.setter
+        # def name(self, name):
+        #      if not name:
+        #         raise ValueError("Missing name")
+        #      self._name = name
+    
+        # @property
+        # def house(self):
+        #     return self._house
+    
+        # @house.setter
+        # def house(self, house):
+        #          if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+        #               raise ValueError("Invalid house")
+        #          self._house = house
